cfa0b79c3d1eb10bc017f7a0acdb55ef373d5fcf
Is-monp <isabellapalencia@uninorte.edu.co>
2024-05-24 12:26:51 -0500

    character controller updated
    -character no longer gets stuck in the tilemap.
    -char can jump and move around the map.
    -Updated physics

--- lab3/Assets/Scrips/cat_moves.cs
+++ lab3/Assets/Scrips/cat_moves.cs
@@ -7,21 +7,25 @@ public class cat_moves : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D mybody;
+
     private void Awake()
     {
         mybody = GetComponent<Rigidbody2D>();
+
     }
+
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        mybody.velocity=new Vector3(horizontalInput*speed,mybody.velocity.y);
+        mybody.velocity = new Vector2(horizontalInput * speed, mybody.velocity.y);
 
         if (horizontalInput > 0.01f)
             transform.localScale = Vector3.one;
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
-
+        if (Input.GetKey("w")&& checkinGround.isGrounded)
+        {
+            mybody.velocity=new Vector2(mybody.velocity.x, speed);
+        }
     }
-
-
 }
