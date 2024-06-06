d0bab0d68a5adefae842e2fa85882ae41e294326
Clau210605 <146784035+Clau210605@users.noreply.github.com>
2024-06-06 11:39:40 -0500

    casi ultimo
    hola

--- lab3/Assets/puntaje.cs
+++ lab3/Assets/puntaje.cs
@@ -1,26 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-
-public class Puntaje : MonoBehaviour
+public class Puntaje : MonoBehaviourPunCallbacks
 {
     private int puntaje;  
-   
-     void Start (){
-         puntaje = 0; 
+
+    void Start ()
+    {
+        puntaje = 0; 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "moneda")
+        if (collision.gameObject.tag == "moneda")
         {
-            puntaje +=10; 
-            Debug.Log(puntaje); 
-
+            photonView.RPC("aumentcoin", RpcTarget.AllBuffered);
         }
+    }
 
-    }
-   
-   
+    [PunRPC]
+    private void aumentcoin()
+    {
+        puntaje += 10; 
+        Debug.Log(puntaje); 
+    }
 }
