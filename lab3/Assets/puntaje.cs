a45f69c96718c9ebd58ab76a6fc7f40967c4ef4d
Clau210605 <146784035+Clau210605@users.noreply.github.com>
2024-06-06 04:01:46 -0500

    monedas
    moneditas

---
+++ lab3/Assets/puntaje.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Puntaje : MonoBehaviour
+{
+    private int score;  
+   
+     void Start (){
+         score = 0; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "moneda")
+        {
+            score +=10; 
+            Debug.Log(score); 
+
+        }
+
+    }
+   
+   
+}
